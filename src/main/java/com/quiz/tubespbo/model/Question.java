05ac9163f8686225a4a5d98e4fe19beafc8618d6
Cleosa <unomiyeng@gmail.com>
2024-12-31 17:49:50 +0700

    V16.0

---
+++ src/main/java/com/quiz/tubespbo/model/Question.java
@@ -0,0 +1,39 @@
+package com.quiz.tubespbo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "question_text")
+    private String questionText;
+
+    @Column(name = "correct_answer")
+    private Boolean correctAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    @ToString.Exclude  // Menghindari rekursi saat menggunakan toString
+    private Quiz quiz;
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", questionText='" + questionText + '\'' +
+                ", correctAnswer=" + correctAnswer +
+                '}';
+    }
+}
+
+
+
