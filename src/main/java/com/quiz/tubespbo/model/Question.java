f067ef7a26b0e94569bca9c7034d52acab97db69
PrabuAditya11 <prabuaditya11@gmail.com>
2024-12-31 17:31:57 +0700

    V14.0 : Session untuk admin

---
+++ src/main/java/com/quiz/tubespbo/model/Question.java
@@ -0,0 +1,30 @@
+package com.quiz.tubespbo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "question")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "question_text")
+    private String questionText;
+
+    @Column(name = "correct_answer")
+    private Boolean correctAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+}
+
+
